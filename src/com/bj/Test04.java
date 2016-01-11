4285a5b105957100288c4d27bbdc0b01e88fe18c
yundong001 <suhua22287@sina.com>
2016-01-11 14:55:58 +0800

    修改Test04
    这次修改是修改Test04

--- src/com/bj/Test04.java
+++ src/com/bj/Test04.java
@@ -5,10 +5,10 @@ public class Test04 {
 	public static void main(String[] args) {
 		System.out.println(4);
 		//add message here.
-		//这里增加注释信息aaa
+		//这里增加注释信息abc
 		
 		
-		System.out.println("好的");
+		System.out.println("好漂亮的风景");
 		
 
 	}
