2bb2a8f46d9f6aa0e72ffee14f430dcd189fbc26
lwch01 <suhua22287@sina.com>
2016-01-11 14:45:50 +0800

    s

--- src/com/bj/Test04.java
+++ src/com/bj/Test04.java
@@ -5,10 +5,10 @@ public class Test04 {
 	public static void main(String[] args) {
 		System.out.println(4);
 		//add message here.
-		//这里增加注释信息
+		//这里增加注释信息aaa
 		
 		
-		System.out.println(6);
+		System.out.println("好的");
 		
 
 	}
